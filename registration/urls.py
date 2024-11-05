b3da41b27071a9035a39595f0f4560ab9664057d
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2024-11-04 18:22:13 -0600

    botones login, cerrar sesion

--- registration/urls.py
+++ registration/urls.py
@@ -6,4 +6,6 @@ app_name = 'registration'
 urlpatterns = [
     path('', index, name='index'),
     path('custom-login/', custom_login, name='custom_login'),
+    path('custom-register/', custom_register, name='custom_register'),
+    path('logout/', logout_view, name='logout'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,6 @@
 from lib2to3.fixes.fix_input import context
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -18,5 +18,12 @@ def custom_login(request):
             context.update({"error": "Usuario o contraseña incorrectos"})
     return render(request, 'registration/custom_login.html', context)
 
+def custom_register(request):
+    context = {}
+    return render(request, 'registration/custom_register.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('registration:index')
 def index(request):
     return render(request, 'registration/index.html')
